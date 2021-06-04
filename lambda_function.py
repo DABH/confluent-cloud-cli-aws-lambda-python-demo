7deecca32b82a0a03caf653a0e03f96004526783
DABH <DABH@users.noreply.github.com>
2021-06-04 01:16:35 -0700

    Fix version

--- lambda_function.py
+++ lambda_function.py
@@ -4,8 +4,8 @@ import subprocess
 import urllib.request
 
 def lambda_handler(event, context):
-    # Download the CLI and make it executable; must use 1.32.0+
-    file_name, headers = urllib.request.urlretrieve("https://s3-us-west-2.amazonaws.com/confluent.cloud/ccloud-cli/binaries/1.32.0/ccloud_1.32.0_linux_amd64")
+    # Download the CLI and make it executable; must use 1.33.0+
+    file_name, headers = urllib.request.urlretrieve("https://s3-us-west-2.amazonaws.com/confluent.cloud/ccloud-cli/binaries/1.33.0/ccloud_1.33.0_linux_amd64")
     subprocess.run(["chmod", "755", file_name])
     
     my_env = os.environ.copy()
